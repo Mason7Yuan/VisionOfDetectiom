382a16a4cbdc8600380d2bc643f3bcf97fcec1ab
Mason7Yuan <78012301+Mason7Yuan@users.noreply.github.com>
2021-04-03 14:14:32 +0800

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Axis color: x is red
+               y is green
+               z is blue.
+*/
+
+public class CameraController : MonoBehaviour
+{
+    private float moveSpeed = 6f;
+    
+    private Rigidbody2D rb;
+    private Camera camSight;
+    private Vector2 ChaVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        camSight = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mosPosition = camSight.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        LookAt2D(transform.position, mosPosition, 100f);
+
+        ChaVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.MovePosition(rb.position + ChaVelocity * Time.fixedDeltaTime);
+        rb.velocity = ChaVelocity;
+    }
+
+    // LookAt function with lerp in 2-D world.
+    private void LookAt2D(Vector2 positionA, Vector2 postionB, float lerpSpeed)
+    { 
+        Vector3 angleAB = new Vector3(0f, 0f, Vector2.SignedAngle(-transform.right, positionA - postionB));
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, transform.eulerAngles + angleAB, lerpSpeed * Time.deltaTime);
+    }
+}
---
+++ FieldOfView.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0f, 360f)] public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector] public List<Transform> visibleTarget = new List<Transform>();
+
+    public float delayTime;
+
+    private void Start()
+    {
+        StartCoroutine("FindTargetWithDelay", delayTime);
+    }
+
+    IEnumerator FindTargetWithDelay(float delayTime)
+    {
+        // Start the method until next scene. So it is always to be true.
+        while (true)
+        {
+            yield return new WaitForSeconds(delayTime);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTarget.Clear();
+
+        Collider2D[] targetInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
+
+        for(var i = 0; i < targetInViewRadius.Length; i++)
+        {
+            Transform target = targetInViewRadius[i].transform;
+            Vector2 dirToTarget = (target.position - transform.position).normalized;
+
+            if (Vector2.Angle(transform.right,dirToTarget) < viewAngle / 2)
+            {
+                float disTotarget = Vector2.Distance(transform.position, target.position);
+
+                if (!Physics2D.Raycast(transform.position, dirToTarget, disTotarget, obstacleMask))
+                {
+                    visibleTarget.Add(target);
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.z;
+        }
+
+        return new Vector3(Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), Mathf.Sin(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
